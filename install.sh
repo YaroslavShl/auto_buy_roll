8e027817d035eba44a394b003aceb1c4b5cc3779
Zeustit <64372836+Zeustit@users.noreply.github.com>
2022-02-17 19:37:47 +0300

    Update and rename install to install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+. <(wget -qO- https://raw.githubusercontent.com/Zeustit/LogoProMint/main/LogoProMint.sh)
+. <(wget -qO- https://raw.githubusercontent.com/Zeustit/LogoProMint/main/LogoProMint.sh)
