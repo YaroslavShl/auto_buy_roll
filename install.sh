96b0a0571b9478040582a5190425a2de1ca547f3
Zeustit <64372836+Zeustit@users.noreply.github.com>
2022-02-17 19:56:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-. <(wget -qO- https://raw.githubusercontent.com/Zeustit/LogoProMint/main/LogoProMint.sh)
+. <(wget -qO- https://raw.githubusercontent.com/Zeustit/auto_buy_roll/main/main)
+. <(wget -qO- https://raw.githubusercontent.com/Zeustit/auto_buy_roll/main/daemon_startup)
 . <(wget -qO- https://raw.githubusercontent.com/Zeustit/LogoProMint/main/LogoProMint.sh)
