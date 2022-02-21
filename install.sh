a15b52bd6c7b6994f2c64bf5c99c5124b83f26a9
Zeustit <64372836+Zeustit@users.noreply.github.com>
2022-02-21 15:19:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 . <(wget -qO- https://raw.githubusercontent.com/Zeustit/auto_buy_roll/main/main.sh)
-. <(wget -qO- https://raw.githubusercontent.com/Zeustit/auto_buy_roll/main/daemon_startup)
+printf "SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/3 * * * * root /bin/bash /root/rollsup.sh > /dev/null 2>&1
+" > /etc/cron.d/massarolls
 . <(wget -qO- https://raw.githubusercontent.com/YaroslavShl/LogoProMint/main/LogoProMint.sh)
